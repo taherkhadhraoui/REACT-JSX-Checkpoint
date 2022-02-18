a5fa5c3bfadffd0d7f1ff2f1b8b75547e5f2f59c
taherkhadhraoui <taherbenhedi@gmail.com>
2022-02-18 16:00:15 +0100

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./style.css";
+import imageInSrc from "./imageinsrc.png";
+import NavbarC from "./Components/Navbar/NavbarC";
+import FooterC from "./Components/Footer/FooterC";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavbarC />
+      <div className="App">
+        <h3 className="title">------- Taher KHADHRAOUI ---------</h3>
+
+        <img className="image" src={imageInSrc} alt="imageinsrc" />
+
+        <img className="image" src="/imageInPublic.jpg" alt="imageinpublic" />
+
+        <video width="320" height="240" controls>
+          <source src="/video-1549022685.mp4" type="video/mp4" />
+        </video>
+      </div>
+      <FooterC />
     </div>
   );
 }
